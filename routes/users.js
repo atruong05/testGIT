071477485b6bd447f57230be2532f1f0f0ca8bbc
unknown <avtruong@WSPS1080.umhs.med.umich.edu>
2014-10-17 11:32:32 -0400

    Test: Added a period in comment

--- routes/users.js
+++ routes/users.js
@@ -39,7 +39,7 @@ router.get(('/var/:source/:supers/:name'), function(req, res){
 	})
 });
 
-//Select two super & name from one source 
+//Select two super & name from one source.
 router.get('/var/:source/:supers1/:name1/:supers2/:name2', function(req, res){
 	var db=req.db;
 	db.collection('var').find({$or:[
